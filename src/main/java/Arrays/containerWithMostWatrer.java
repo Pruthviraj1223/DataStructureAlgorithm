6b65d478941b23a2f129460f952f354ce6d84de7
root <pj73270@gmail.com>
2022-08-18 22:54:51 +0530

    containerWithMostWater

---
+++ src/main/java/Arrays/containerWithMostWatrer.java
@@ -0,0 +1,54 @@
+package Arrays;
+
+
+class container{
+
+    public int maxArea(int[] height) {
+
+        if (height.length==0){
+
+            return 0;
+
+        }
+
+        int left =0;
+        int right = height.length-1;
+
+        int area = 0;
+
+        while(left<right){
+
+
+            area = Math.max(area,(right-left) * Math.min(height[left],height[right]));
+
+
+
+            if (height[left]<height[right]){
+
+
+                left = left + 1;
+
+            }else{
+
+                right = right - 1;
+
+            }
+
+        }
+
+
+        return area;
+
+
+
+
+    }
+
+}
+
+public class containerWithMostWatrer {
+
+    public static void main(String[] args) {
+
+    }
+}
