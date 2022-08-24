0288a30bcac5145115363f0942b8459a2019ccf1
root <pj73270@gmail.com>
2022-08-24 18:52:19 +0530

    MaximumSubarraySizeOfSizeK

---
+++ src/main/java/Arrays/FirstNegativeElementOfWindowSizeK.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+class Negative{
+
+    public void NegativeEleSizeK(int []nums,int k){
+
+
+
+
+    }
+
+}
+
+
+public class FirstNegativeElementOfWindowSizeK {
+    public static void main(String[] args) {
+
+    }
+}
