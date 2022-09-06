900134606fc49c58f566ec383418acdffaa02722
Pruthviraj <pj73270@gmail.com>
2022-09-06 22:09:22 +0530

    allocateMinimumNumberOfMaximumPages and aggresiveCows and singleElement

--- src/main/java/BinarySearch/allocateMinimumNumberOfMaximumPages.java
+++ src/main/java/BinarySearch/allocateMinimumNumberOfMaximumPages.java
@@ -1,31 +1,31 @@
 package BinarySearch;
 
-class allocate{
+class allocate {
 
-    boolean isPossible(int []nums,int midSum,int students){
+    boolean isPossible(int[] nums, int midSum, int students) {
 
         int allocateStudents = 1;
 
-        int pages =0;
+        int pages = 0;
 
         for (int index = 0; index < nums.length; index++) {
 
-            if(nums[index] > midSum){
+            if (nums[index] > midSum) {
 
                 return false;
 
-            }else if (pages + nums[index] > midSum){
+            } else if (pages + nums[index] > midSum) {
 
                 allocateStudents++;
 
                 pages = nums[index];
 
-                if (allocateStudents > students){
+                if (allocateStudents > students) {
 
-                    return  false;
+                    return false;
                 }
 
-            }else {
+            } else {
 
                 pages += nums[index];
 
@@ -36,39 +36,40 @@ class allocate{
         return true;
 
 
-
     }
 
-    public int minimumNoOfPages(int []nums,int student){
+    public int minimumNoOfPages(int[] nums, int student) {
 
         int n = nums.length;
 
-        int sum =0;
+        int sum = 0;
+
         for (int num : nums) {
 
             sum += num;
 
         }
 
-        int low=0;
+        int low = 0;
+
         int high = sum;
 
-        int ans=-1;
+        int ans = -1;
 
-        while (low<=high){
+        while (low <= high) {
 
-            int mid = (low + high) /2;
+            int mid = (low + high) / 2;
 
-            if(isPossible(nums, mid, student)){
+            if (isPossible(nums, mid, student)) {
 
                 ans = mid;
 
-                high = mid-1;
+                high = mid - 1;
 
 
-            }else {
+            } else {
 
-                low = mid+1;
+                low = mid + 1;
 
             }
 
---
+++ src/main/java/BinarySearch/singleElement.java
@@ -0,0 +1,85 @@
+package BinarySearch;
+
+class single {
+
+    public int singleEle(int[] nums, int low, int high) {
+
+        if (low == high) {
+            return nums[low];
+
+        }
+
+        int mid = (low + high) / 2;
+
+        if (mid % 2 == 1) {
+
+            if (nums[mid] == nums[mid - 1]) {
+
+                return singleEle(nums, mid + 1, high);
+
+            } else {
+
+                return singleEle(nums, low, mid - 1);
+
+            }
+
+        } else {
+
+
+            if (nums[mid] == nums[mid + 1]) {
+
+                return singleEle(nums, mid + 2, high);
+
+            } else {
+
+                return singleEle(nums, low, mid);
+
+            }
+
+        }
+
+    }
+
+    public int singleNonDuplicate(int[] nums) {
+
+        int start = 0, end = nums.length - 1, mid;
+
+        if (nums.length == 1) // If only one element is in the array
+            return nums[0];
+
+        if (nums[start]  != nums[start + 1]) // If the first element
+            return nums[start];
+
+        if (nums[end] != nums[end- 1]) // If Last element is the element
+            return nums[end];
+
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+
+            if (nums[mid] != nums[mid - 1]  && nums[mid] != nums[mid + 1])
+                return nums[mid];
+
+                // CASE 2 and CASE 3
+
+            else if ((nums[mid] == nums[mid + 1] && mid % 2 == 0)
+                    || (nums[mid] == nums[mid - 1] && mid % 2 != 0))
+                start = mid + 1;
+                // CASE 4 and CASE 5
+
+            else
+                end = mid - 1;
+
+        }
+
+        return -1;
+
+    }
+
+}
+
+public class singleElement {
+
+    public static void main(String[] args) {
+
+    }
+}
