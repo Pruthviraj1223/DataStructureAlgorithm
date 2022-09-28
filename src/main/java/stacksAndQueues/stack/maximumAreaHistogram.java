a366a723bb362a8d0951dd702f9e339d2119bc0b
Pruthviraj <pj73270@gmail.com>
2022-09-28 23:34:08 +0530

    maximumRectangleAreaInBinaryMatrix

--- src/main/java/stacksAndQueues/stack/maximumAreaHistogram.java
+++ src/main/java/stacksAndQueues/stack/maximumAreaHistogram.java
@@ -98,6 +98,8 @@ public class maximumAreaHistogram {
 
         var left = new Histogram().nextSmallerElementLeftSide(height);
 
+        right.set(10, 1);
+
         int maxArea = -1;
 
         System.out.println("left " + left);
