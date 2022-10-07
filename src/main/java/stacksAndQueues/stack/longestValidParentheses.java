796332283eed3ecc0b32db34a161f20ca1c5a393
Pruthviraj <pj73270@gmail.com>
2022-10-07 19:37:16 +0530

    longestValidParentheses

---
+++ src/main/java/stacksAndQueues/stack/longestValidParentheses.java
@@ -0,0 +1,139 @@
+package stacksAndQueues.stack;
+
+import java.util.Stack;
+
+class longestParentheses{
+    public int longestValidParenthesesStack(String s) {
+
+        if(s.isEmpty()){
+
+            return 0;
+
+        }
+
+
+        java.util.Stack<Integer> stack = new Stack<>();
+
+
+        stack.push(-1);
+
+        int maxLength=0;
+
+
+        for(int index=0;index<s.length();index++){
+
+
+
+            if(s.charAt(index)=='('){
+
+                stack.push(index);
+            }
+
+            else{
+
+                // understand conditions
+
+                if(!stack.isEmpty()){
+
+                    stack.pop();
+
+                }
+
+                if(!stack.isEmpty()){
+
+                    maxLength = Math.max(maxLength,index-stack.peek());
+
+                }else{
+
+                    stack.push(index);
+
+                }
+
+            }
+
+        }
+
+        return maxLength;
+
+
+    }
+
+    public int longestValidParenthesesTwoPointer(String s) {
+
+        if(s.isEmpty()){
+
+            return 0;
+
+        }
+
+
+        int left =0;
+        int right=0;
+        int maxLength=0;
+
+
+        for(int index=0;index<s.length();index++){
+
+            if(s.charAt(index)=='('){
+
+                left++;
+
+            }else{
+
+                right++;
+
+            }
+
+            // ()())((((()))))
+
+            if(left==right){
+
+                maxLength = Math.max(maxLength,2 * left);
+
+
+            }else if(right>left){       // understand this conditions
+
+                left = right =0;
+
+            }
+
+        }
+
+        left = right =0;
+
+
+        for(int index=s.length()-1;index>=0;index--){
+
+            if(s.charAt(index)=='('){
+
+                left++;
+
+            }else{
+
+                right++;
+
+            }
+
+            if(left==right){
+
+                maxLength = Math.max(maxLength,2 * left);
+
+            }else if(right<left){
+
+                left = right =0;
+
+            }
+        }
+
+        return maxLength;
+
+    }
+}
+
+public class longestValidParentheses {
+    public static void main(String[] args) {
+
+        new longestParentheses().longestValidParenthesesTwoPointer("()()((((()))))");
+
+    }
+}
