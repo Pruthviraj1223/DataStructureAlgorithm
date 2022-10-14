41ad754ec60b0fa348833295692ffa6db40ac8c9
Pruthviraj <pj73270@gmail.com>
2022-10-14 23:40:33 +0530

    sum of min and max of windows size k

---
+++ src/main/java/stacksAndQueues/queue/sumOfMinimumAndMaximumOfWindowKSize.java
@@ -0,0 +1,92 @@
+package stacksAndQueues.queue;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class sum{
+
+    public void sumOfMinMax(int []nums,int k){
+
+        Deque<Integer> min = new LinkedList<>();    // it will be in increasing order , peek ele would be the smallest
+
+        Deque<Integer> max = new LinkedList<>();    // it will be in decreasing order , peek ele would be the maximum
+
+        int index;
+        int sum=0;
+
+        for (index = 0; index < k; index++) {
+
+            int currentEle = nums[index];
+
+            while (!min.isEmpty() && nums[min.peekLast()]>=currentEle){
+
+                min.pop();
+
+            }
+
+            while (!max.isEmpty() && nums[max.peekLast()]<=currentEle){
+
+                max.pop();
+
+            }
+
+            min.add(index);
+
+            max.add(index);
+
+        }
+
+        for (;index < nums.length && !min.isEmpty() && !max.isEmpty(); index++) {
+
+            sum += nums[min.peek()] + nums[max.peek()];
+
+            int currentEle = nums[index];
+
+            while (!min.isEmpty() && min.peek() <= index-k){
+
+                min.pop();
+
+            }
+
+            while (!max.isEmpty() && max.peek() <= index-k){
+
+                max.pop();
+
+            }
+
+
+            while (!min.isEmpty() && nums[min.peek()] >= currentEle){
+
+                min.pop();
+
+            }
+
+            while (!max.isEmpty() && nums[max.peek()] <= currentEle){
+
+                max.pop();
+
+            }
+
+            min.addLast(index);
+
+            max.addLast(index);
+
+        }
+
+        sum += nums[min.peek()] + nums[max.peek()];
+
+        System.out.println(sum);
+
+    }
+
+
+}
+
+public class sumOfMinimumAndMaximumOfWindowKSize {
+    public static void main(String[] args) {
+
+        new sum().sumOfMinMax(new int[]{2, 5, -1, 7, -3, -1, -2},3);
+
+    }
+}
