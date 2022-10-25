5702738bd6f3518c943ab4ca646b507e11cf026c
Pruthviraj <pj73270@gmail.com>
2022-10-25 22:46:36 +0530

    delete Without Head Pointer

---
+++ src/main/java/Linked_List/deleteWithoutHeadPointer.java
@@ -0,0 +1,23 @@
+package Linked_List;
+
+class delete{
+    public void deleteNode(ListNode node) {
+
+        ListNode next = node.next;
+
+        node.val = next.val;
+
+        node.next = next.next;
+
+        next.next = null;
+
+
+    }
+}
+
+
+public class deleteWithoutHeadPointer {
+    public static void main(String[] args) {
+
+    }
+}
