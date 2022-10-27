c4a727baf9afe24fd98ba6d79d995d1865c1ec92
Pruthviraj <pj73270@gmail.com>
2022-10-27 20:04:05 +0530

    partition Around Given Value

---
+++ src/main/java/Linked_List/partitionAroundGivenValue.java
@@ -0,0 +1,45 @@
+package Linked_List;
+
+class pratition {
+
+    public ListNode partition(ListNode head, int x) {
+
+        ListNode dummyHead = new ListNode(-1);
+        ListNode temp = dummyHead;
+
+        ListNode dummyHead2 = new ListNode(-1);
+        ListNode temp2 = dummyHead2;
+
+        while (head != null) {
+
+
+            if (head.val < x) {
+
+
+                temp.next = head;
+                temp = temp.next;
+
+            } else {
+
+                temp2.next = head;
+                temp2 = temp2.next;
+
+            }
+
+            head = head.next;
+        }
+
+        temp.next = dummyHead2.next;
+
+        temp2.next = null;
+
+        return dummyHead.next;
+
+    }
+}
+
+public class partitionAroundGivenValue {
+    public static void main(String[] args) {
+
+    }
+}
