b6b8bfc22d89ab4ec23a3517c808341f0a0730af
Pruthviraj <pj73270@gmail.com>
2022-11-05 20:22:50 +0530

    KMP added

--- src/main/java/string/KMP_LPS.java
+++ src/main/java/string/KMP_LPS.java
@@ -2,6 +2,49 @@ package string;
 
 public class KMP_LPS {
 
+    void KMPSearch(String pat, String txt) {
+
+        int M = pat.length();
+
+        int N = txt.length();
+
+        int[] lps = new int[M];
+
+        int j = 0; // index for pat[]
+
+        longestPrefix(pat, lps);
+
+        int i = 0;
+
+        while ((N - i) >= (M - j)) {
+
+            if (pat.charAt(j) == txt.charAt(i)) {
+
+                j++;
+
+                i++;
+            }
+
+            if (j == M) {
+
+                System.out.println("Found pattern "
+                        + "at index " + (i - j));
+
+                j = lps[j - 1];
+
+            } else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+
+                if (j != 0)
+
+                    j = lps[j - 1];
+
+                else
+
+                    i = i + 1;
+            }
+        }
+    }
+
     public String longestPrefix(String s) {
 
         // abcdefabclpoabcbczetabcde
