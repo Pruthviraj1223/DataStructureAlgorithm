ae5827e94bb501e1d55866e99d01426adeec1274
Pruthviraj <pj73270@gmail.com>
2022-11-15 14:40:57 +0530

    print all sub-sequence

---
+++ src/main/java/string/printAllSubSequence.java
@@ -0,0 +1,29 @@
+package string;
+
+public class printAllSubSequence {
+
+    static void print(String s, String ans) {
+
+        if (s.length() == 0) {
+
+            System.out.println(ans);
+            return;
+
+        }
+
+
+        print(s.substring(1), ans + s.charAt(0));
+
+
+        print(s.substring(1), ans);
+
+    }
+
+
+    public static void main(String[] args) {
+
+        print("abcd", "");
+
+
+    }
+}
