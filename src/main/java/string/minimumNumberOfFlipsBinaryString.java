aa57f3808f440dd112451ce7e7895aaadf1ebf28
Pruthviraj <pj73270@gmail.com>
2022-11-17 20:12:06 +0530

    min number of flipped count to make binary string alternative

---
+++ src/main/java/string/minimumNumberOfFlipsBinaryString.java
@@ -0,0 +1,42 @@
+package string;
+
+import java.awt.*;
+
+public class minimumNumberOfFlipsBinaryString {
+
+
+    static char flip(Character ch) {
+
+        return (ch == '0') ? '1' : '0';
+
+    }
+
+    static int getFlippedCount(String s,char expected) {
+
+        int flippedCount = 0;
+
+        for (int index = 0; index < s.length(); index++) {
+
+            if (s.charAt(index) != expected) {
+
+                flippedCount++;
+
+            }
+
+            expected = flip(expected);
+
+        }
+
+        return flippedCount;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        var ans = Math.min(getFlippedCount("0001010111",'0'),getFlippedCount("0001010111",'1'));
+
+        System.out.println(ans);
+
+    }
+}
