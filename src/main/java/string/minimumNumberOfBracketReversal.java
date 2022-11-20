b864e5cdd1c138c9dded17a2fca8e3395ec08834
Pruthviraj <pj73270@gmail.com>
2022-11-20 22:37:17 +0530

    min parentheses need to added to make valid (Leetcode)

--- src/main/java/string/minimumNumberOfBracketReversal.java
+++ src/main/java/string/minimumNumberOfBracketReversal.java
@@ -37,7 +37,6 @@ public class minimumNumberOfBracketReversal {
 
     static int reversalUsingTemp(String s) {
 
-        Stack<Character> stack = new Stack<>();
         int ans = 0;
         int temp = 0;
 
@@ -72,10 +71,47 @@ public class minimumNumberOfBracketReversal {
 
     }
 
+    public int minAddToMakeValidLeetcode(String s) {
+
+        int ans = 0;
+        int temp = 0;
+
+        for (int index = 0; index < s.length(); index++) {
+
+            if (s.charAt(index) == '(') {
+
+                temp++;
+
+            } else {
+
+                if (temp == 0) {
+
+                    ans++;
+
+                } else {
+
+                    temp--;
+
+                }
+            }
+        }
+
+        if (temp > 0) {
+
+            ans += temp;
+
+        }
+
+        return ans;
+
+    }
+
 
     public static void main(String[] args) {
 
         //min number of bracket need to reverse to make balanced sting
+
+        // leetcode version is added . It's bit different
 
     }
 }
