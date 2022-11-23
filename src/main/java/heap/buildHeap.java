2424a752d0b50f0b6b94e0f99dfe3a5b38b8e03d
Pruthviraj <pj73270@gmail.com>
2022-11-23 23:23:27 +0530

    build heap

---
+++ src/main/java/heap/buildHeap.java
@@ -0,0 +1,28 @@
+package heap;
+
+import java.util.Arrays;
+
+public class buildHeap {
+
+    static void buildMaxHeap(int[] arr) {
+
+        int n = arr.length;
+
+        for (int index = n / 2 - 1; index >= 0; index--) {
+
+            heapifyArray.heapify(arr, index);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {16, 11, 18, 5, 13, 17};
+
+        buildMaxHeap(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
