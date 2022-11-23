760767e734c0d000fade1a4a930c8315f779cfdd
Pruthviraj <pj73270@gmail.com>
2022-11-23 23:28:13 +0530

    refactor and add util

--- src/main/java/heap/heapifyArray.java
+++ src/main/java/heap/heapifyArray.java
@@ -30,20 +30,11 @@ public class heapifyArray {
 
         if (largest != index) {
 
-            swap(arr, largest, index);
+            util.swap(arr, largest, index);
 
             heapify(arr, largest);
 
         }
-
-
-    }
-
-    private static void swap(int[] arr, int largest, int index) {
-
-        int temp = arr[largest];
-        arr[largest] = arr[index];
-        arr[index] = temp;
 
     }
 
