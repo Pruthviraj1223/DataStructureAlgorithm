f20abbcad940eb97bbd9ba19d52aa126f332784b
pruthviraj <pruthviraj.jadeja@motadata.com>
2022-12-12 09:32:00 +0530

    add leetcode solution . Revision.

--- src/main/java/string/rotateString.java
+++ src/main/java/string/rotateString.java
@@ -2,9 +2,52 @@ package string;
 
 public class rotateString {
 
-    boolean rotate(String a,String b){
+    public boolean rotateStrings(String A, String B) {
 
-        return a.length()==b.length() && (a+a).contains(b);
+        // Leetcode solution
+
+        if (A == null || B == null) {
+            //throw exception on A and B both being null?
+            return false;
+        }
+
+        if (A.length() != B.length()) {
+            return false;
+        }
+
+        if (A.length() == 0) {
+            return true;
+        }
+
+        for (int index = 0; index < A.length(); index++) {
+
+            if (checkIfRotated(A, B, index)) {
+
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+    private boolean checkIfRotated(String A, String B, int rotation) {
+
+        for (int index = 0; index < A.length(); index++) {
+
+            if (A.charAt(index) != B.charAt((index + rotation) % B.length())) {
+
+                return false;
+
+            }
+        }
+
+        return true;
+
+    }
+
+    boolean rotate(String a, String b) {
+
+        return a.length() == b.length() && (a + a).contains(b);
 
         // O(N^2)
 
@@ -16,6 +59,12 @@ public class rotateString {
 
 
     public static void main(String[] args) {
+
+        // Also watch out checkIfStringIsRotatedByTwoPlaces.
+
+        //https://leetcode.com/problems/rotate-string/
+
+        // if you want to know how place rotation the second string have , we can also get
 
     }
 }
