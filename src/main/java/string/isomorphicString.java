c90ee1741a59f017c2ebaf8bf32325644e409948
pruthviraj <pruthviraj.jadeja@motadata.com>
2022-12-16 22:42:24 +0530

    Revision...

--- src/main/java/string/isomorphicString.java
+++ src/main/java/string/isomorphicString.java
@@ -6,26 +6,25 @@ public class isomorphicString {
 
     public boolean isIsomorphic(String s1, String s2) {
 
-        if(s1.length()!=s2.length()){
+        if (s1.length() != s2.length()) {
             return false;
         }
 
-        HashMap<Character,Character> mapper = new HashMap<>();
+        HashMap<Character, Character> mapper = new HashMap<>();
 
-        for(int index=0;index<s1.length();index++){
+        for (int index = 0; index < s1.length(); index++) {
 
-            if(mapper.containsKey(s1.charAt(index))){
+            if (mapper.containsKey(s1.charAt(index))) {
 
-                if(s2.charAt(index)!= mapper.get(s1.charAt(index))){
+                if (s2.charAt(index) != mapper.get(s1.charAt(index))) {
 
                     return false;
 
                 }
 
-            } else if (!mapper.containsValue(s2.charAt(index))){
+            } else if (!mapper.containsValue(s2.charAt(index))) {
 
-                mapper.put(s1.charAt(index),s2.charAt(index));
-
+                mapper.put(s1.charAt(index), s2.charAt(index));
 
             } else {
 
@@ -40,6 +39,13 @@ public class isomorphicString {
     }
 
     public static void main(String[] args) {
+
+        //https://leetcode.com/problems/isomorphic-strings/
+        // s = "egg", t = "add"
+
+
+        // Given two strings s and t, determine if they are isomorphic.
+        // Two strings s and t are isomorphic if the characters in s can be replaced to get t.
 
     }
 }
