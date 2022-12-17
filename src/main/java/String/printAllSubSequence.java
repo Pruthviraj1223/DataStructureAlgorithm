589ec2832e42e659d57fe535a15b34095fb6d13f
pruthviraj <pruthviraj.jadeja@motadata.com>
2022-12-17 20:50:26 +0530

    Revision

--- src/main/java/String/printAllSubSequence.java
+++ src/main/java/String/printAllSubSequence.java
@@ -2,30 +2,36 @@ package String;
 
 public class printAllSubSequence {
 
-    static void print(String s, String ans) {
+    static void printSubSequence(String s, String ans) {
+
+        // this is question needs your peace, awareness and a lot of involvement
+        // don't get confused
+        // just remember the concept of pick and don't pick character
 
         if (s.length() == 0) {
 
-            System.out.println(ans);
+            System.out.println("Result : " + ans);
             return;
 
         }
 
+        // pick character
+        System.out.println("1 -> " + "s : " + s  + ", ans : " + ans);
 
-        print(s.substring(1), ans + s.charAt(0));
+        printSubSequence(s.substring(1), ans + s.charAt(0));
 
+        // don't pick character
+        System.out.println("2 -> " + "s : " + s  + ", ans : " + ans);
 
-        print(s.substring(1), ans);
+        printSubSequence(s.substring(1), ans);
 
     }
-
 
     public static void main(String[] args) {
 
         // Time complexity - O(2^n)
 
-        print("abcd", "");
-
+        printSubSequence("abcd", "");
 
     }
 }
