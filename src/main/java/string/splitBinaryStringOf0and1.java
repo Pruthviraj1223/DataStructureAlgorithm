457a849e82a975bf8e32b8d4bd127d3134a86744
pruthviraj <pruthviraj.jadeja@motadata.com>
2022-12-17 15:33:56 +0530

    Revision...

--- src/main/java/string/splitBinaryStringOf0and1.java
+++ src/main/java/string/splitBinaryStringOf0and1.java
@@ -6,7 +6,7 @@ public class splitBinaryStringOf0and1 {
 
         int count0 = 0, count1 = 0;
 
-        int cnt = 0;
+        int answer = 0;
 
         for (int index = 0; index < n; index++) {
 
@@ -22,7 +22,7 @@ public class splitBinaryStringOf0and1 {
 
             if (count0 == count1) {
 
-                cnt++;
+                answer++;
 
             }
         }
@@ -33,10 +33,12 @@ public class splitBinaryStringOf0and1 {
 
         }
 
-        return cnt;
+        return answer;
     }
 
     public static void main(String[] args) {
+
+        // https://www.geeksforgeeks.org/split-the-binary-string-into-substrings-with-equal-number-of-0s-and-1s/
 
     }
 }
