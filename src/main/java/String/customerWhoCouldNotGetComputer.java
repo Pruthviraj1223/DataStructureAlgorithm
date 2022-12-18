1c649bdb148d766c03aaa687ef8132e123cec2ca
pruthviraj <pruthviraj.jadeja@motadata.com>
2022-12-18 22:05:23 +0530

    Revision

--- src/main/java/String/customerWhoCouldNotGetComputer.java
+++ src/main/java/String/customerWhoCouldNotGetComputer.java
@@ -4,21 +4,24 @@ public class customerWhoCouldNotGetComputer {
 
     static int runCustomerSimulation(int n, char[] seq) {
 
+        // ex : ABBAJJKZKZ              answer : 0
+        // ex : GACCBDDBAGEE            answer : not 0
+
         char[] seen = new char[26];
 
-        int res = 0;
+        int unsatisfied = 0;
 
         int occupied = 0; // To keep track of occupied computers
 
-        for (int i = 0; i < seq.length; i++) {
+        for (int index = 0; index < seq.length; index++) {
 
-            int ind = seq[i] - 'A';
+            int ind = seq[index] - 'A';
 
             if (seen[ind] == 0) {       // 0 means not in the cafe
 
                 seen[ind] = 1;          // 1 means in the cafe , but not using computer
 
-                if (occupied < n) {
+                if (occupied < n) {     // two possibility : if there are enough computer then customer satisfied else unsatisfied
 
                     occupied++;
 
@@ -26,26 +29,31 @@ public class customerWhoCouldNotGetComputer {
 
                 } else {
 
-                    res++;
+                    unsatisfied++;           // it means customer arrived to the shop , but didn't get the computer
 
                 }
 
             } else {
 
                 if (seen[ind] == 2) {
+
                     occupied--;
+
                 }
 
                 seen[ind] = 0;
-
             }
         }
 
-        return res;
+        return unsatisfied;
 
     }
 
     public static void main(String[] args) {
+
+        //https://www.geeksforgeeks.org/function-to-find-number-of-customers-who-could-not-get-a-computer/
+
+        // nice thinking , not specific to string but a god one
 
     }
 }
