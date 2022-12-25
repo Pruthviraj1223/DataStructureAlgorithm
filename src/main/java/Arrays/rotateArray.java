74538bef2e73ab03e0f4a36394bd921e0dd2f895
pruthviraj <pruthviraj.jadeja@motadata.com>
2022-12-25 17:47:01 +0530

    Revision

--- src/main/java/Arrays/rotateArray.java
+++ src/main/java/Arrays/rotateArray.java
@@ -1,48 +1,42 @@
 package Arrays;
 
-class Solution12 {
+class Rotate {
 
+    static void reverse(int[] nums, int start, int end) {
 
-    void reverse(int[] nums, int start, int end) {
-
-        int i = 0;
-        while (start <= end) {
+        while (start < end) {
 
             int temp = nums[start];
+
             nums[start] = nums[end];
+
             nums[end] = temp;
 
             start++;
             end--;
 
-
         }
-
-
     }
-
 
     public void rotate(int[] nums, int k) {
 
+        int n = nums.length;
 
-        k = k % nums.length;
+        k = k % nums.length;                    // must condition
 
-        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, n - 1);
 
         reverse(nums, 0, k - 1);
 
-        reverse(nums, k, nums.length - 1);
-
-
+        reverse(nums, k, n - 1);
     }
 }
 
 public class rotateArray {
-
     public static void main(String[] args) {
 
+        //https://leetcode.com/problems/rotate-array/
 
     }
-
 
 }
