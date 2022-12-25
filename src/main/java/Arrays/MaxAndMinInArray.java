09bd41aad491fbb2df8866e996b29b6bcda7010b
pruthviraj <pruthviraj.jadeja@motadata.com>
2022-12-25 15:23:40 +0530

    Revision

---
+++ src/main/java/Arrays/MaxAndMinInArray.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+public class MaxAndMinInArray {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{3, 54, 2, 78, 11};
+
+//        Arrays.sort(nums);         // you can sort the array like this
+//
+//        for(int a:nums){
+//            System.out.println(a);
+//        }
+
+        int min = nums[0];
+
+        int max = nums[0];
+
+        for (int num : nums) {
+
+            if (num > max) {
+                max = num;
+            }
+
+            if (num < min) {
+                min = num;
+            }
+
+        }
+    }
+}
