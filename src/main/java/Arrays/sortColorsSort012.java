e93db4fb1bcf6688c08d7ba05c7e5d5fa5b3b1fe
pruthviraj <pruthviraj.jadeja@motadata.com>
2022-12-25 17:36:17 +0530

    Revision

--- src/main/java/Arrays/sortColorsSort012.java
+++ src/main/java/Arrays/sortColorsSort012.java
@@ -2,27 +2,26 @@ package Arrays;
 
 public class sortColorsSort012 {
 
-
     public static void main(String[] args) {
 
+        // good question
 
         // sort colors
         // sort 0 , 1 , 2
-        // dutch national flag
+        // Dutch national flag
+
+        // [2,0,2,1,1,0]
+        // 0 0 2 1 1 2
 
         int[] nums = new int[]{1, 2};// it will be given
-
 
         int low = 0;
         int mid = 0;
         int high = nums.length - 1;
 
-
         while (mid <= high) {
 
-
             if (nums[mid] == 0) {
-
 
                 int temp = nums[low];
 
@@ -30,14 +29,11 @@ public class sortColorsSort012 {
 
                 nums[mid] = temp;
 
-
                 low++;
 
-                mid++;
-
-
+                mid++;                                   // the reason for mid++ : when low == mid then low gets ++ and mid has to be ++. For ex : initially low and mid 0
+                                                         // another reason for mid ++ : once you swap , element at mid will be always 1.
             } else if (nums[mid] == 2) {
-
 
                 int temp = nums[high];
 
@@ -45,23 +41,13 @@ public class sortColorsSort012 {
 
                 nums[mid] = temp;
 
-
                 high--;
-
 
             } else {
 
-
                 mid++;
 
-
             }
-
-
         }
-
-
     }
-
-
 }
