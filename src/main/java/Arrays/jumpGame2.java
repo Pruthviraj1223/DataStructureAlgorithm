c378df2a20de59fd8f9b4d8a306f13980c07c439
pruthviraj <pruthviraj.jadeja@motadata.com>
2022-12-29 21:38:10 +0530

    jump game 2

--- src/main/java/Arrays/jumpGame2.java
+++ src/main/java/Arrays/jumpGame2.java
@@ -1,31 +1,36 @@
-package Arrays;//package Arrays;
-//
-//public class jumpGame2 {
-//    public static void main(String[] args) {
-//        public int jump(int[] A) {
-//
-//            int steps = 0;
-//            int tempMax= 0;
-//            int max = 0;
-//
-//            for(int i=0; i<A.length-1; i++) {
-//
-//                max = Math.max(max, i+A[i]);
-//
-//                if( i == tempMax ) {
-//
-//                    steps++;
-//
-//                    tempMax = max;
-//
-//                }
-//
-//            }
-//
-//            return steps;
-//
-//
-//        }
-//    }
-//    }
-//}
+package Arrays;
+
+public class jumpGame2 {
+
+    static int jump(int[] nums) {
+
+        int steps = 0;
+        int upcomingIndex = 0;
+        int max = 0;
+
+        for (int index = 0; index < nums.length - 1; index++) {
+
+            max = Math.max(nums[index] + index, max);
+
+            if (index == upcomingIndex) {
+
+                steps++;
+
+                upcomingIndex = max;
+
+            }
+
+        }
+
+        return steps;
+    }
+
+    public static void main(String[] args) {
+
+        //https://leetcode.com/problems/jump-game-ii/submissions/867584420/
+
+        var result = jump(new int[]{1,2,3});
+
+    }
+
+}
