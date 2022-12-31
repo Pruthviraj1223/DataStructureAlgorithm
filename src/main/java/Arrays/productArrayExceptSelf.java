f1b8acf378e19f7dcd0b9695e65d9ea89c771535
pruthviraj <pruthviraj.jadeja@motadata.com>
2022-12-31 16:30:34 +0530

    Revision

--- src/main/java/Arrays/productArrayExceptSelf.java
+++ src/main/java/Arrays/productArrayExceptSelf.java
@@ -1,8 +1,9 @@
 package Arrays;
 
 
-class pruthvi {
-    void prod(int[] nums) {
+class productOfArray {
+
+    int[] product(int[] nums) {
 
         // MUST understand two approaches
         // tech dose
@@ -21,7 +22,6 @@ class pruthvi {
 
             left[index] = nums[index] * left[index - 1];
 
-
         }
 
         for (int index = n - 2; index >= 0; index--) {
@@ -30,26 +30,29 @@ class pruthvi {
 
         }
 
-        int[] ans = new int[left.length];
+        int[] result = new int[left.length];
 
         for (int index = 1; index < left.length - 1; index++) {
 
-            ans[index] = left[index - 1] * right[index + 1];
+            result[index] = left[index - 1] * right[index + 1];
 
         }
 
-        ans[0] = right[1];
+        // result[index] = left[index - 1] * right[index + 1]; same formula applies below
+        // but at 0 position you cant do left[index-1] and same for last position
 
-        ans[left.length - 1] = left[left.length - 2];
+        result[0] = right[1];
 
-        // Above solution works well
+        result[left.length - 1] = left[left.length - 2];
 
-        // below solution is optimized
+        return result;
+    }
 
+    int[] productMoreEfficient(int []nums){
 
-        int[] left1 = new int[nums.length];
+        int[] left = new int[nums.length];
 
-        int n1 = nums.length;
+        int n = nums.length;
 
         left[0] = nums[0];
 
@@ -59,7 +62,12 @@ class pruthvi {
 
         }
 
-        int prod = 1; // instead of right array use prod variable
+        int prod = 1; // instead of using right array use prod variable
+
+        // from back of the array it will get started to multiply with current element
+        // prod will maintain the multiplication of all element in right side , so we don't need right array
+        // so instead of left[index - 1] * right[index + 1] in above solution
+        // we can do left[index - 1] * prod
 
         for (int index = n - 1; index > 0; index--) {
 
@@ -71,6 +79,7 @@ class pruthvi {
 
         left[0] = prod;
 
+        return left;
     }
 }
 
@@ -80,6 +89,9 @@ public class productArrayExceptSelf {
 
         int[] n = {1, 2, 3, 4};
 
-        new pruthvi().prod(n);
+        new productOfArray().product(n);
+
+        //https://leetcode.com/problems/product-of-array-except-self/
+
     }
 }
