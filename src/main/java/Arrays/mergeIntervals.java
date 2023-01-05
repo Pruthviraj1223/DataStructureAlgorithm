ed941893b3c267110923293556178b1d67fe9773
pruthviraj <pruthviraj.jadeja@motadata.com>
2023-01-05 09:05:44 +0530

    Revision

--- src/main/java/Arrays/mergeIntervals.java
+++ src/main/java/Arrays/mergeIntervals.java
@@ -8,28 +8,28 @@ public class mergeIntervals {
     public static int[][] main(String[] args) {
 
 
-        int [][]intervals = new int[][]{{1,2},{3,5}};
+        int[][] intervals = new int[][]{{1, 2}, {3, 5}};
 
 
-        Arrays.sort(intervals,(a, b) -> Integer.compare(a[0],b[0]));
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
 
         LinkedList<int[]> list = new LinkedList<>();
 
 
-        for(int index=0;index<intervals.length;index++){
+        for (int index = 0; index < intervals.length; index++) {
 
 
-            if(list.isEmpty() || list.getLast()[1] < intervals[index][0]){
+            if (list.isEmpty() || list.getLast()[1] < intervals[index][0]) {
 
 
                 list.add(intervals[index]);
 
 
-            }else{
+            } else {
 
 
-                list.getLast()[1] = Math.max(intervals[index][1],list.getLast()[1]);
+                list.getLast()[1] = Math.max(intervals[index][1], list.getLast()[1]);
 
             }
 
