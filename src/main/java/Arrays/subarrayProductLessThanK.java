5feb45ca3612e52b41cd9e59193090943e01a2d3
pruthviraj <pruthviraj.jadeja@motadata.com>
2023-01-07 19:46:01 +0530

    Revision

--- src/main/java/Arrays/subarrayProductLessThanK.java
+++ src/main/java/Arrays/subarrayProductLessThanK.java
@@ -7,30 +7,27 @@ class SubarrayProduct {
 
         int n = nums.length;
 
-        if (n==0 || k<=1){
+        if (n == 0 || k <= 1) {
 
             return 0;
 
         }
 
-        int left=0;
+        int left = 0;
 
-        int prod=1;
+        int prod = 1;
 
         int ans = 0;
 
-        for(int right=0;right<n;right++){
-
+        for (int right = 0; right < n; right++) {
 
             prod = prod * nums[right];
 
-
-            while(prod>=k){
+            while (prod >= k) {
 
                 prod = prod / nums[left];
 
                 left++;
-
 
             }
 
@@ -47,7 +44,7 @@ public class subarrayProductLessThanK {
 
     public static void main(String[] args) {
 
-        new SubarrayProduct().numSubarrayProductLessThanK(new int[]{10,5,2,6},100);
+        new SubarrayProduct().numSubarrayProductLessThanK(new int[]{10, 5, 2, 6}, 100);
 
     }
 }
