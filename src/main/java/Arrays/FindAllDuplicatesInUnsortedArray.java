56e91bc4990b32178443271502f72b93b9ef0aa1
pruthviraj <pruthviraj.jadeja@motadata.com>
2023-01-07 22:19:47 +0530

    Revision

--- src/main/java/Arrays/FindAllDuplicatesInUnsortedArray.java
+++ src/main/java/Arrays/FindAllDuplicatesInUnsortedArray.java
@@ -7,19 +7,19 @@ class Solution {
 
     public List<Integer> findDuplicates(int[] nums) {
 
-        List<Integer> result = new ArrayList();
+        List<Integer> result = new ArrayList<>();
 
-        if (nums.length <= 1){
+        if (nums.length <= 1) {
 
             return result;
 
         }
 
-        for(int index=0;index<nums.length;index++){
+        for (int index = 0; index < nums.length; index++) {
 
             int idx = Math.abs(nums[index]) - 1;
 
-            if (nums[idx]<0){
+            if (nums[idx] < 0) {
 
                 result.add(Math.abs(nums[index]));
 
@@ -36,4 +36,13 @@ class Solution {
 }
 
 public class FindAllDuplicatesInUnsortedArray {
+    public static void main(String[] args) {
+
+        // https://leetcode.com/problems/find-the-duplicate-number/submissions/873453283/
+
+        // above link is for one duplicate number
+        // code is for multiple duplicate number
+
+
+    }
 }
