7df9b9677a192e184e1aa01c19db47ab257011f8
pruthviraj <pruthviraj.jadeja@motadata.com>
2023-01-22 23:17:34 +0530

    Node util..

---
+++ src/main/java/Tree/NodeUtil.java
@@ -0,0 +1,19 @@
+package Tree;
+
+class TreeNode{
+
+    int val;
+    TreeNode left,right;
+
+    TreeNode(int val,TreeNode left,TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class NodeUtil {
+    public static void main(String[] args) {
+
+    }
+}
