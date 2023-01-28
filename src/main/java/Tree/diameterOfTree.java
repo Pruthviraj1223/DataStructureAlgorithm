95e747045dd7b9e87e7c5bb29b74814a98920cb5
pruthviraj <pruthviraj.jadeja@motadata.com>
2023-01-28 16:39:42 +0530

    diameter of tree

---
+++ src/main/java/Tree/diameterOfTree.java
@@ -0,0 +1,37 @@
+package Tree;
+
+public class diameterOfTree {
+
+    public int maxDiameter(TreeNode root, int[] d) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = maxDiameter(root.left, d);
+        int rightHeight = maxDiameter(root.right, d);
+
+        d[0] = Math.max(d[0], leftHeight + rightHeight);
+
+        return 1 + Math.max(leftHeight, rightHeight);
+
+    }
+
+    public int diameter(TreeNode root) {
+
+        int[] d = new int[1];
+
+        maxDiameter(root, d);
+
+        return d[0];
+
+    }
+
+    public static void main(String[] args) {
+
+        //https://leetcode.com/problems/diameter-of-binary-tree/submissions/
+
+        // here we are just taking the maximum of left and right height
+
+    }
+}
