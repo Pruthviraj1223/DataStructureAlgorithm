2360766d97b670084d04380b9f5476662b94c68b
pruthviraj <pruthviraj.jadeja@motadata.com>
2023-01-28 16:39:07 +0530

    check tree is balanced or not

---
+++ src/main/java/Tree/CheckTreeSelfBalancedOrNot.java
@@ -0,0 +1,42 @@
+package Tree;
+
+public class CheckTreeSelfBalancedOrNot {
+
+    public boolean isBalanced(TreeNode root) {
+
+        return dfs(root) != -1;
+
+    }
+
+    public int dfs(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int lh = dfs(root.left);
+
+        if (lh == -1) {
+            return -1;
+        }
+
+        int rh = dfs(root.right);
+
+        if (rh == -1) {
+            return -1;
+        }
+
+        if (Math.abs(lh - rh) > 1) {
+            return -1;
+        }
+
+        return 1 + Math.max(lh, rh);
+
+    }
+
+    public static void main(String[] args) {
+
+        //https://leetcode.com/problems/balanced-binary-tree/
+
+    }
+}
