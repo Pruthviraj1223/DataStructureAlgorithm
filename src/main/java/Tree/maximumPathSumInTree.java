b8a3f542f7777be4c52096c1e353e5ab3935a54d
pruthviraj <pruthviraj.jadeja@motadata.com>
2023-01-28 16:39:52 +0530

    maximum path sum

---
+++ src/main/java/Tree/maximumPathSumInTree.java
@@ -0,0 +1,37 @@
+package Tree;
+
+public class maximumPathSumInTree {
+
+    public int maxPathSum(TreeNode node, int[] d) {
+
+        if (node == null) {
+            return 0;
+        }
+
+        int leftVal = maxPathSum(node.left, d);
+        int rightVal = maxPathSum(node.right, d);
+
+        d[0] = Math.max(d[0], leftVal + rightVal + node.val);
+
+        return node.val + Math.max(leftVal, rightVal);
+
+    }
+
+    public int maxSum(TreeNode root) {
+
+        int[] d = new int[1];
+
+        maxPathSum(root, d);
+
+        return d[0];
+
+    }
+
+    public static void main(String[] args) {
+
+        //https://leetcode.com/problems/binary-tree-maximum-path-sum/
+
+        // here instead of calculating height we will calculate the sum
+
+    }
+}
