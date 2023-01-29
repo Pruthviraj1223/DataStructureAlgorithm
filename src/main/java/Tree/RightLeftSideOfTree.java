5098f48ae48ccb76c5e694b0c723519a81aae767
pruthviraj <pruthviraj.jadeja@motadata.com>
2023-01-29 20:27:25 +0530

    Right / Left side view traversal

---
+++ src/main/java/Tree/RightLeftSideOfTree.java
@@ -0,0 +1,39 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RightLeftSideOfTree {
+
+    public List<Integer> rightSide(TreeNode node, List<Integer> result, int currDepth) {
+
+        if (node == null) {
+            return result;
+        }
+
+        if (currDepth == result.size()) {                   // must understand
+            result.add(node.val);
+        }
+
+        rightSide(node.right, result, currDepth + 1);
+        rightSide(node.left, result, currDepth + 1);
+
+        return result;
+
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+
+        rightSide(root, result, 0);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        // https://leetcode.com/problems/binary-tree-right-side-view/submissions/
+
+    }
+}
