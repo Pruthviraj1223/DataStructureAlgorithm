6ff8c244a834694101a15c0ebf5533f610d4f021
pruthviraj <pruthviraj.jadeja@motadata.com>
2023-01-31 20:08:26 +0530

    Path From Root To Node

---
+++ src/main/java/Tree/PathFromRootToNode.java
@@ -0,0 +1,46 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathFromRootToNode {
+
+    public boolean getPath(TreeNode node, List<Integer> result, int target) {
+
+        if (node == null) {
+            return false;
+        }
+
+        result.add(node.val);
+
+        if (node.val == target) {
+            return true;
+        }
+
+        if (getPath(node.left, result, target) || getPath(node.right, result, target)) {
+            return true;
+        }
+
+        result.remove(result.size() - 1);
+
+        return false;
+
+    }
+
+
+    public List<Integer> path(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+
+        getPath(root, result, 7);
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+
+        // aditya verma
+
+    }
+}
