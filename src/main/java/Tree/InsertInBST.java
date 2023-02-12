3a7bc1b8387fbd404ea3a3048519db7a69102de1
pruthviraj <pruthviraj.jadeja@motadata.com>
2023-02-12 21:52:37 +0530

    Added new method for insertion in BST

--- src/main/java/Tree/InsertInBST.java
+++ src/main/java/Tree/InsertInBST.java
@@ -7,6 +7,20 @@ public class InsertInBST {
         NodeUtil.insertBST(data);
     }
 
+    public TreeNode insertRecursion(TreeNode root, int val)
+    {
+        // this just more elegant way
+
+        if(root == null) return new TreeNode(val);
+
+        if(root.val > val) root.left = insertRecursion(root.left, val);
+
+        else root.right = insertRecursion(root.right, val);
+
+        return root;
+    }
+
+
     public static void main(String[] args) {
 
         insert(new int[]{10,5,13,7,12,6});
