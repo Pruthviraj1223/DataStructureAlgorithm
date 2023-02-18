d780ec1af4b913b0f7a221e299f61758cb9ed4df
pruthviraj <pruthviraj.jadeja@motadata.com>
2023-02-19 00:14:06 +0530

    Predecessor Successor

---
+++ src/main/java/Tree/PredecessorSuccessor.java
@@ -0,0 +1,55 @@
+package Tree;
+
+public class PredecessorSuccessor {
+
+    public static TreeNode Successor(TreeNode root,TreeNode target)
+    {
+        TreeNode successor = null;
+
+        while (root != null)
+        {
+            if (target.val >= root.val)
+            {
+                root = root.right;
+            }
+            else
+            {
+                successor = root;
+
+                root = root.left;
+            }
+        }
+
+        return successor;
+    }
+
+    public static TreeNode Predecessor(TreeNode root,TreeNode target)
+    {
+        TreeNode predecessor = null;
+
+        while (root != null)
+        {
+            if (target.val > root.val)
+            {
+                predecessor = root;
+
+                root = root.right;
+            }
+            else
+            {
+                root = root.left;
+            }
+        }
+
+        return predecessor;
+    }
+
+    public static void main(String[] args) {
+
+
+        // it is exactly similar to ceil and floor
+
+        // https://www.youtube.com/watch?v=SXKAD2svfmI&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=51
+
+    }
+}
