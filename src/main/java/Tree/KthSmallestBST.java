3a754c4615667bd7cafb1e6b201dc5b965bf5562
pruthviraj <pruthviraj.jadeja@motadata.com>
2023-02-18 16:22:09 +0530

    Kth Smallest BST

---
+++ src/main/java/Tree/KthSmallestBST.java
@@ -0,0 +1,31 @@
+package Tree;
+
+public class KthSmallestBST {
+
+    int count = 0;
+    int result = Integer.MIN_VALUE;
+
+    public void traverse(TreeNode root, int k) {
+
+        if(root == null) return;
+
+        traverse(root.left, k);
+
+        count ++;
+
+        if(count == k) result = root.val;
+
+        traverse(root.right, k);
+    }
+
+    public int kthSmallest(TreeNode root, int k) {
+
+        traverse(root, k);
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
